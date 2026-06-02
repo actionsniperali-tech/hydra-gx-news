c620c9247187993d5aed6e66e87055e911a06ff2
actionsniperali-tech <action.sniper.ali@gmail.com>
2026-06-02 05:34:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-
 FEEDS = [
-    "https://vigiato.net/feed/"
+    "https://vigiato.net/feed/",
+    "https://digiato.com/feed/",
+    "https://zoomg.ir/feed/"
 ]
-
 def send_message(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {
@@ -23,11 +23,17 @@ def get_news():
 
     for feed_url in FEEDS:
         feed = feedparser.parse(feed_url)
+for item in feed.entries[:3]:
+    title = item.title
+    link = item.link
 
-        for item in feed.entries[:3]:
-            title = item.title
-            link = item.link
+    image = None
 
+    # گرفتن عکس از RSS (اگر وجود داشت)
+    if "media_content" in item:
+        image = item.media_content[0]["url"]
+    elif "enclosures" in item and len(item.enclosures) > 0:
+        image = item.enclosures[0].get("url")
             msg = f"""🎮 {title}
 
 📺 تماشا 👈 {link}
@@ -41,4 +47,14 @@ HYDRA GX"""
 
 if __name__ == "__main__":
     for news in get_news():
-        send_message(news)
+      if image:
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto",
+        data={
+            "chat_id": CHANNEL_ID,
+            "photo": image,
+            "caption": msg
+        }
+    )
+else:
+    send_message(msg)
